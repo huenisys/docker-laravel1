2dd6937b6ffa6b7bc921850ec726103b522b66fe
huenisys <paul@huenits.com>
2019-10-11 13:39:37 +0800

    add entrypoint

---
+++ docker/start-container.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# make sure /.composer exists and writable
+if [ ! -d /.composer ]; then
+  mkdir /.composer
+fi
+
+chmod -R ugo+rw /.composer
+
+# run things different whether prod or dev env, etc
+
+# Run command or start supervisord
+if [ $# -gt 0 ]; then
+  # If command is passed, run it
+  exec "$@"
+else
+  # Otherwise start supervisord
+  /usr/bin/supervisord
+fi
